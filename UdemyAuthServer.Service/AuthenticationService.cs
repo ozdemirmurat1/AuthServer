1eb56721ffe27e9fc4303c78af9a0514616aaafc
ozdemirmurat1 <ozdemirmu16@itu.edu.tr>
2023-05-03 18:49:44 +0300

    AddUserService

--- UdemyAuthServer.Service/AuthenticationService.cs
+++ UdemyAuthServer.Service/AuthenticationService.cs
@@ -75,14 +75,43 @@ namespace UdemyAuthServer.Service
             return Response<ClientTokenDto>.Success(token, 200);
         }
 
-        public Task<Response<TokenDto>> CreateTokenByRefreshToken(string refreshToken)
+        public async Task<Response<TokenDto>> CreateTokenByRefreshToken(string refreshToken)
         {
-            throw new NotImplementedException();
+            var existRefreshToken = await _userRefreshTokenService.Where(x => x.Code == refreshToken).SingleOrDefaultAsync();
+
+            if (existRefreshToken == null)
+                return Response<TokenDto>.Fail("Refresh Token not found", 404, true);
+
+            var user = await _userManager.FindByIdAsync(existRefreshToken.UserId);
+
+            if (user == null)
+                return Response<TokenDto>.Fail("User Id not found", 404, true);
+
+            var tokenDto = _tokenService.CreateToken(user);
+
+            existRefreshToken.Code = tokenDto.RefreshToken;
+            existRefreshToken.Expiration=tokenDto.RefreshTokenExpiration;
+
+            await _unitOfWork.CommitAsync();
+
+            return Response<TokenDto>.Success(tokenDto, 200);
+
+
         }
 
-        public Task<Response<NoDataDto>> RevokeRefreshToken(string refreshToken)
+        public async Task<Response<NoDataDto>> RevokeRefreshToken(string refreshToken)
         {
-            throw new NotImplementedException();
+            // refresh token null a set edilecek.
+
+            var existRefreshToken = await _userRefreshTokenService.Where(x => x.Code == refreshToken).SingleOrDefaultAsync();
+
+            if (existRefreshToken == null)
+                return Response<NoDataDto>.Fail("Refresh token not found", 404, true);
+
+            _userRefreshTokenService.Remove(existRefreshToken);
+            await _unitOfWork.CommitAsync();
+
+            return Response<NoDataDto>.Success(200);
         }
     }
 }
