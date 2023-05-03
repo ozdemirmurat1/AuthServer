2b806c4a70554ddc1274c229437131c71c66157f
ozdemirmurat1 <ozdemirmu16@itu.edu.tr>
2023-05-03 22:57:48 +0300

    AddStartUp

--- UdemyAuthServer.API/Startup.cs
+++ UdemyAuthServer.API/Startup.cs
@@ -1,7 +1,9 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -13,6 +15,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using UdemyAuthServer.Core.Configuration;
+using UdemyAuthServer.Core.Models;
+using UdemyAuthServer.Core.Repositories;
+using UdemyAuthServer.Core.Services;
+using UdemyAuthServer.Core.UnitOfWork;
+using UdemyAuthServer.Data;
+using UdemyAuthServer.Data.Repositories;
+using UdemyAuthServer.Service;
+using UdemyAuthServer.Service.Services;
 
 namespace UdemyAuthServer.API
 {
@@ -28,6 +38,34 @@ namespace UdemyAuthServer.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // DI Register
+
+            services.AddScoped<IAuthenticationService, AuthenticationService>();
+            services.AddScoped<IUserService, UserService>();
+            services.AddScoped<ITokenService,TokenService>();
+            services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+            // araya virgül koymamýzýn sebebi 2 tane generic type alamasý
+            services.AddScoped(typeof(IServiceGeneric<,>), typeof(ServiceGeneric<,>));
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+            services.AddDbContext<AppDbContext>(options =>
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("SqlServer"), sqlOptions =>
+                {
+                    //UdemyAuthServer.Data katmanýnda migration iþlemi yapýcaz o yüzden bu ismi kullandýk
+                    sqlOptions.MigrationsAssembly("UdemyAuthServer.Data");
+                });
+            });
+
+            services.AddIdentity<UserApp, IdentityRole>(Opt =>
+            {
+                Opt.User.RequireUniqueEmail = true;
+                Opt.Password.RequireNonAlphanumeric = false;
+
+            }).AddEntityFrameworkStores<AppDbContext>().AddDefaultTokenProviders();
+            // Þifre sýrfýrlama iþlemlerinde default token saðlamak için .AddDefaultTokenProviders ekledik.
+
+
             // CustomTokenOption TokenOptions daki parametreleri doldurup bize bir nesne örneði verecek.
 
             services.Configure<CustomTokenOption>(Configuration.GetSection("TokenOption"));
